db5c9b343e44cb7ec4a8e6b6ec93fc5c47e96bae
Maciej Bartkowiak <108934199+MBartkowiakSTFC@users.noreply.github.com>
2023-04-24 17:07:37 +0100

    Remove the original directory structure

--- Src/Chemistry/__init__.py
+++
@@ -1,9 +0,0 @@
-from MDANSE.Chemistry.Databases import AtomsDatabase, MoleculesDatabase, NucleotidesDatabase, ResiduesDatabase
-
-ATOMS_DATABASE = AtomsDatabase()
-
-MOLECULES_DATABASE = MoleculesDatabase()
-
-RESIDUES_DATABASE = ResiduesDatabase()
-
-NUCLEOTIDES_DATABASE = NucleotidesDatabase()
